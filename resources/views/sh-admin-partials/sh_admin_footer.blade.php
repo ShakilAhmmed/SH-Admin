87ebef3e8467e644d5109832fbca0c299f4bd7fb
Rahib Hasan <rahibhasan689009@gmail.com>
2020-01-27 02:20:20 +0600

    neccessary files

---
+++ resources/views/sh-admin-partials/sh_admin_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="footer text-muted">
+	&copy; 2020. <a href="#">SH-ADMIN</a> by <a href="#" target="_blank">~~~~~</a>
+</div>
+
+
+{{-- content div from main page --}}
+			</div>
+			<!-- /main content -->
+		</div>
+		<!-- /page content -->
+	</div>
+	<!-- /page container -->
+</body>
+</html>
+
